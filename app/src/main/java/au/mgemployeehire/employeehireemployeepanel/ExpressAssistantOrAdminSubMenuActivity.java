b48bbf0d3bfea9ee5d36892f0702c13c9f376f98
Md. Talat Mahmud <md.talatcse@gmail.com>
2021-03-17 17:17:57 +0600

    updated with new requirement march 17, 2021

---
+++ app/src/main/java/au/mgemployeehire/employeehireemployeepanel/ExpressAssistantOrAdminSubMenuActivity.java
@@ -0,0 +1,14 @@
+package au.mgemployeehire.employeehireemployeepanel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ExpressAssistantOrAdminSubMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_express_assistant_or_admin_sub_menu);
+    }
+}
